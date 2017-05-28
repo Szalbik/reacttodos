c054228baa5ca80db3f2371edb682f4c2b713266
Damian Szalbierz <szalbierz.d.k@prosper24.pl>
2017-05-28 14:11:41 +0200

    Fix reducer todos

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 class TodoList extends Component {
-  renderTodoList(todo){
-    return <li key={todo.id}>{todo.text}</li>;
-  }
-
   render() {
-    if( this.props.todos.langth === 0 ) {
+    if( this.props.todos.length === 0 ) {
       return <div>Add Todos</div>;
     }
+
+    console.log(this.props.todos)
 
     return(
       <ul>
--- src/reducers/todos_reducer.js
+++ src/reducers/todos_reducer.js
@@ -1,16 +1,10 @@
-import ADD_TODO from '../actions';
+import { ADD_TODO } from '../actions/index';
 
 const todos = (state = [], action) => {
   console.log('Reducer text: ',action);
   switch (action.type) {
     case ADD_TODO:
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text
-        }
-      ]
+      return [...state, { id: action.id,text: action.text }];
     default:
       return state
   }
